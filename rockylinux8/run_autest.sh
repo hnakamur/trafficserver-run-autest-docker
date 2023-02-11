3f0c403273a32478f16bd5341e3b2c896f83ce0c
Hiroaki Nakamura <hnakamur@gmail.com>
2023-02-11 22:18:50 +0900

    Make all tests pass on rockylinux8

---
+++ rockylinux8/run_autest.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+#set +e
+# We want to pick up the OpenSSL-QUIC version of curl in /opt/bin.
+# The HTTP/3 AuTests depend upon this, so update the PATH accordingly.
+export PATH=/opt/bin:${PATH}
+
+mkdir -p ${WORKSPACE}/output/${GITHUB_BRANCH}
+
+# Make all files which will be created during tests writable by nobody user.
+umask 0000
+
+if [ ${SHARDCNT:-0} -le 0 ]; then
+	./autest.sh --ats-bin ${ATS_PREFIX:-/tmp/ats}/bin/ --sandbox /tmp/sandbox "$@" || true
+else
+	testsall=( $( find . -iname "*.test.py" | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}' ) )
+
+	testsall=( $(
+		for el in  "${testsall[@]}" ; do
+			echo $el
+		done | sort) )
+	ntests=${#testsall[@]}
+
+	shardsize=$((${ntests} / ${SHARDCNT}))
+	[ 0 -ne $((${ntests} % ${shardsize})) ] && shardsize=$((${shardsize} + 1))
+	shardbeg=$((${shardsize} * ${SHARD}))
+	sliced=${testsall[@]:${shardbeg}:${shardsize}}
+	./autest.sh --ats-bin ${ATS_PREFIX:-/tmp/ats}/bin/ --sandbox /tmp/sandbox -f ${sliced[@]} || true
+fi
+
+if [ -n "$(ls -A /tmp/sandbox/)" ]; then
+	cp -rf /tmp/sandbox/ ${WORKSPACE}/output/${GITHUB_BRANCH}/
+	sudo chmod -R 777 ${WORKSPACE}/output
+	exit 1
+else
+	if [ ${SHARDCNT:-0} -le 0 ]; then
+		sudo touch ${WORKSPACE}/output/${GITHUB_BRANCH}/No_autest_failures
+	else
+		sudo touch ${WORKSPACE}/output/${GITHUB_BRANCH}/No_autest_failures_shard_${SHARD}
+	fi
+	sudo chmod -R 777 ${WORKSPACE}/output
+	exit 0
+fi
